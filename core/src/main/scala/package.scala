8756508ecd21b7036727b8b34ce5ff42631086e3
softprops <d.tangren@gmail.com>
2013-12-03 08:14:55 -0500

    update package doc

--- core/src/main/scala/package.scala
+++ core/src/main/scala/package.scala
@@ -1,13 +1,14 @@
-/** Deffered defines a set of primatives for deferring
- *  the execution of some task
+/** Odelay defines a set of primatives for delaying
+ *  the execution of operations.
  * 
  *  This is differs from scala.concurrent.Futures in that
- *  the execution of a task it defined in terms of delay defined
- *  as a scala.concurrent.duration.Duration. The defferal of a
- *  task may also may be cancelled.
+ *  the execution of an operation will not occur until a provided delay, specified
+ *  as a scala.concurrent.duration.Duration. The delay of a
+ *  task may also may be cancelled. Operations may also be executed after a series
+ *  of delays, also represented by scala.concurrent.duration.Durations.
  *
- *  These primatives can be used to complement the usage of scala.concurrent.Futures by defineing
+ *  These primatives can be used to complement the usage of scala.concurrent.Futures by defining
  *  a deterministic delay for the future operation as well as a way
  *  to cancel the future operation.
  */
-package object deferred
+package object odelay
