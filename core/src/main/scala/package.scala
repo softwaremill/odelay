db47d17e55d3f6da9365b9e08294a4a73e7016c0
softprops <d.tangren@gmail.com>
2014-04-24 23:23:22 -0400

    update package docs

--- core/src/main/scala/package.scala
+++ core/src/main/scala/package.scala
@@ -1,17 +1,17 @@
-/** Odelay defines a set of primatives for delaying
+/** Odelay defines a set of primitives for delaying
  *  the execution of operations.
  * 
  *  This is differs from scala.concurrent.Futures in that
  *  the execution of an operation will not occur until a provided delay, specified
  *  as a scala.concurrent.duration.Duration. The delay of a
- *  task may also may be cancelled. Operations may also be executed after a series
+ *  task may also may be canceled. Operations may also be executed after a series
  *  of delays, also represented by scala.concurrent.duration.Durations.
  *
- *  These primatives can be used to complement the usage of scala.concurrent.Futures by defining
+ *  These primitives can be used to complement the usage of scala.concurrent.Futures by defining
  *  a deterministic delay for the future operation as well as a way
  *  to cancel the future operation.
  *
- *  An odelay.Timeout is the result of a delay, defines a future which may be used to trigger dependant actions
+ *  An odelay.Delay represents a delayed operation and defines a future method which may be used to trigger dependent actions
  *  and delay cancellations.
  */
 package object odelay
