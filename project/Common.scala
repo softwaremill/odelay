a401a4ae2c315043748f266db1e0967bd7d7b80e
softprops <d.tangren@gmail.com>
2014-07-04 16:15:15 -0400

    move most of this stuff into build.sbt

--- project/Common.scala
+++ project/Common.scala
@@ -1,14 +1,10 @@
+import sbt.{ file, Defaults, Project }
 //http://www.scala-sbt.org/0.13.2/docs/Getting-Started/Multi-Project.html
 object Common {
-  import sbt._, sbt.Keys._
+  import sbt.Keys._
   def module(mod: String) =
     Project(mod, file(mod), 
             settings = Defaults.defaultSettings ++ Seq(
-              name := s"odelay-$mod",
-              crossScalaVersions := Seq("2.9.3", "2.10.4", "2.11.0"),
-              scalaVersion := crossScalaVersions.value.head,
-              scalacOptions ++= Seq(Opts.compile.deprecation),
-              licenses := Seq(("MIT",  url("https://github.com/softprops/odelay/blob/%s/LICENSE"
-                                           .format(version.value))))) ++
+              name := s"odelay-$mod") ++
             bintray.Plugin.bintraySettings)
 }
